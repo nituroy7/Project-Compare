4276b7d5acf37f3973d24f302e9de6ac307c1918
nituroy7 <nitu_prodhani@yahoo.com>
2017-06-19 10:42:58 -0500

    2nd commit with lots of update

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+var mymodal;
 $(document).ready(function() {
     // Initialize Firebase
     var config = {
@@ -13,8 +14,33 @@ $(document).ready(function() {
 
     // Initial Values
     var item = "";
-    var zipCode = "";
+    //function to call the you tube video 
+    mymodal = function(productName, num) {
+        $.ajax({
+            url: "https://www.googleapis.com/youtube/v3/search?part=snippet&q=" + productName + " review" + "&key=AIzaSyB8g6i8y1SPFbIcJw9flTwk7VEFXYWA5MY",
+            context: document.body
+        }).done(function(response) {
+            $('#dialog').empty();
+            var videoId = response.items[0].id.videoId;
+            var embedCode = "<iframe id='youframe' width=\"100%\" height=\"315\"></iframe>";
 
+            $('#dialog').append(embedCode);
+            $('#dialog').dialog({
+                autoOpen: false,
+                show: "fade",
+                hide: "fade",
+                modal: true,
+                height: 'auto',
+                width: 'auto',
+                resizable: true,
+                title: productName,
+                open: function(ev, ui) {
+                    $('#youframe').attr('src', 'https://www.youtube.com/embed/' + videoId);
+                }
+            });
+            $('#dialog').dialog('open');
+        });
+    }
 
     // Capture Button Click
     $("#searchItem").on("click", function() {
@@ -42,51 +68,83 @@ $(document).ready(function() {
                 withCredentials: false
             }
         }).done(function(response) {
-            console.log(response);
+            //creates the header group
+            var hgroup = $('<hgroup>').attr({ class: 'mb20' });
 
-            var hgroup = $('<hgroup />').attr({
-                class: 'mb20'
-            });
-            $('<h2 />').html('Search Results').appendTo(hgroup);
-            hgroup.appendTo($('#main'));
+            //building out the search result header tag
+            hgroup.append($('<h1>').html('Search Results'));
+
+            var searchCount = $('<span>')
+                .html('<strong class = \'text-danger\'>' + response.totalResults + '</strong>')
+                .append(' results were found for the search: ')
+                .append('<strong class = \'text-danger\'>' + item + '</ strong>');
+
+            hgroup.append($('<h2>').attr({ class: 'lead' }).append(searchCount));
+            //appending the search result header tag to the hqroup.
+            $('#searchResultsDiv').empty();
+
+            $('#searchResultsDiv').append(hgroup);
 
             // this will clear the search results for every new search
             $('section').empty();
-            var section = $('<section />').attr({
+            var section = $('<section>').attr({
                 class: 'col-xs-12 col-sm-6 col-md-12'
             });
+
             // populating the search results
             for (var i = 0; i < response.items.length; i++) {
-                var article = $('<article />').attr({
+                var article = $('<article>').attr({
                     class: 'search-result row'
                 });
+                //creating images
+                var imageLink = $('<a>').attr({
+                    href: response.items[i].productUrl,
+                    title: response.items[i].name,
+                    class: 'thumbnail'
+                }).append($('<img>').attr({
+                    src: response.items[i].mediumImage
+                }));
 
-                $('<img />').attr({
-                        src: response.items[i].mediumImage
-                    })
-                    .appendTo($('<a />').attr({
-                        href: response.items[i].productUrl,
-                        title: response.items[i].name,
-                        class: 'thumbnail'
-                    })).appendTo($('<div />').attr({
-                        class: 'col-xs-12 col-sm-12 col-md-3'
-                    })).appendTo(article);
+                var imageDiv = $('<div>').attr({
+                    class: 'col-xs-12 col-sm-12 col-md-3'
+                }).append(imageLink);
 
-                var div = $('<div />').attr({
-                    class: 'col-xs-12 col-sm-12 col-md-7'
+                article.append(imageDiv);
+
+                //creating the listDiv to hold the price, ratings
+                var listDiv = $('<div>').attr({
+                    class: "col-xs-12 col-sm-12 col-md-2"
                 });
 
-                $('<a />').html(response.items[i].name).attr({
+                $('<p>').html("Original Price: " + response.items[i].msrp).appendTo(listDiv);
+                $('<p>').html("Sale Price: " + response.items[i].salePrice).appendTo(listDiv);
+                $('<p>').html("Customer Ratings: " + response.items[i].customerRating).appendTo(listDiv);
+                $('<img>').attr({ src: response.items[i].customerRatingImage }).appendTo(listDiv);
+                $('<p>').html("Reviews: " + response.items[i].numReviews).appendTo(listDiv);
+                listDiv.appendTo(article);
+
+                //creating the product link with short description
+                var descriptionDiv = $('<div>').attr({
+                    class: 'col-xs-12 col-sm-12 col-md-7',
+                    id: 'descriptionDiv' + '_' + i
+                });
+
+                var productLink = $('<h4>').append($('<a>').html(response.items[i].name).attr({
                     href: response.items[i].productUrl,
                     title: response.items[i].name
-                }).appendTo($('<h3 />')).appendTo(div);
+                }));
+                descriptionDiv.append(productLink);
 
-                $('<p />').html(response.items[i].shortDescription).appendTo(div);
+                var produceDesc = $('<p>').html(response.items[i].shortDescription);
+                descriptionDiv.append(produceDesc);
 
-                div.appendTo(article);
+                // link for youtube
+                descriptionDiv.append($("<a onclick='mymodal(\"" + (response.items[i].name).replace(/"/g, '\\"') + "," + i + "\")'>").html("<h4 class='youtube' id='youtube" + i + " '>find more with a youtube review, click here!</h4>"));
+
+                article.append(descriptionDiv);
                 article.appendTo(section);
             }
-            section.appendTo($('#main'));
+            section.appendTo($('#searchResultsDiv'));
         });
     });
 });
